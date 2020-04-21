8a6fbdb3b4fe36c45761295d51e5940d2d69f225
TellvandenBergh <tellvandenbergh@gmail.com>
2020-04-21 18:43:43 +0200

    trying-spotify-key

--- src/components/SearchSong.vue
+++ src/components/SearchSong.vue
@@ -1,15 +1,47 @@
 <template>
   <div>
-    <iframe src="https://open.spotify.com/embed/album/1DFixLWuPkv3KT3TnV35m3" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+    <button @click="getAccessToken">click</button>
+    <iframe
+      src="https://open.spotify.com/embed/album/1DFixLWuPkv3KT3TnV35m3"
+      width="300"
+      height="380"
+      frameborder="0"
+      allowtransparency="true"
+      allow="encrypted-media"
+    ></iframe>
   </div>
 </template>
 
 <script>
+import axios from "axios";
+var bodyFormData = new FormData();
+bodyFormData.set("grant_type", "client_credentials");
+
+const key =
+  "ZjI3YzZjYmEwNmJlNGM3NjkxZWFkYWRlYjQwYmI4YTg6M2VkOGFkYjE1MjlkNGQzNWI0ZGQzYWJmY2IxZWM2Mzg=";
 export default {
-  name: "SearchSong"
-}
+  name: "SearchSong",
+  methods: {
+    async getAccessToken() {
+      console.log("button clicked");
+      axios({
+        method: "post",
+        url: `https://accounts.spotify.com/api/token`,
+        form: {
+          grant_type: "client_credentials"
+        },
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+          Authorization: "Basic " + key // client id and secret from env
+        },
+        json: true
+      })
+        .then(console.log)
+        .catch(console.log);
+    }
+  }
+};
 </script>
 
 <style>
-
 </style>

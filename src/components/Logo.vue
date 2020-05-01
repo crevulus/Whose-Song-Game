3caef47274c93518a2375ed3bd09c3c114223e52
TellvandenBergh <tellvandenbergh@gmail.com>
2020-05-01 10:30:06 +0200

    title changed

--- src/components/Logo.vue
+++ src/components/Logo.vue
@@ -3,9 +3,7 @@
     <router-link :to="{ name: 'home' }" class="inline-block">
       <div class="flex items-center">
         <img src="@/assets/img/atium_mark@2x.png" class="-mt-1 h-16" />
-        <h2
-          class="text-3xl font-header text-gray-700 mb-0 leading-none"
-        >Whose Favourite Song is That?</h2>
+        <h2 class="text-3xl font-header text-gray-700 mb-0 leading-none">My Anthem</h2>
       </div>
     </router-link>
   </section>
